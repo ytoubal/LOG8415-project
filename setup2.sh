bb32b87eb773f29a2e0323112f3fc71c1ce7c054
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-12-17 15:40:24 -0500

    rollback setup2

--- setup2.sh
+++ setup2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 TOPIC_NAME="s3-topic"
 TOPIC_ARN=$(aws sns create-topic --name $TOPIC_NAME | py -c "import sys, json; print(json.load(sys.stdin)['TopicArn'])")
-END_POINT="<ENTER THE PUBLIC DNS OF THE EC2 INSTANCE>"
+END_POINT="<ENTER YOUR END POINT URL>"
 echo $TOPIC_ARN
 aws sns subscribe --topic-arn $TOPIC_ARN --protocol http --notification-endpoint $END_POINT
-
